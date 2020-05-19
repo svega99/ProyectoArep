a5eb67b8318eb53a950e5a062908c3f8690251f2
Mateo Gonzalez <matteu20@hotmail.com>
2020-05-19 13:37:31 -0500

    Cliente con tiempo

--- src/main/java/edu/escuelaing/arep/realtimebuses/ClienteSpark/SparkWeb.java
+++ src/main/java/edu/escuelaing/arep/realtimebuses/ClienteSpark/SparkWeb.java
@@ -9,6 +9,10 @@ package edu.escuelaing.arep.realtimebuses.ClienteSpark;
 import java.io.BufferedReader;
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import spark.*;
 import static spark.Spark.*;
 /**
@@ -45,13 +49,26 @@ public class SparkWeb {
         return pageContent;
     }
 
-    private static String resultsPage(Request req, Response res) throws IOException {
+    private static String resultsPage(Request req, Response res) throws IOException, InterruptedException {
         
         BufferedReader in = null;
-
+        
+        /**ArrayList Longitud = new ArrayList();
+        ArrayList Latitud = new ArrayList();*/
+        
+        List<?> Latitud = new ArrayList<>(Arrays.asList(4.755457,4.747224,4.728404,4.724769,4.722101,4.715146,4.705795,4.697984,4.694586,4.685802,4.677188,4.672927,4.666853,4.659138,4.651337,4.645482,4.638397,4.626211,4.622228,4.617293));
+        List<?> Longitud = new ArrayList<>(Arrays.asList(-74.046128,-74.047179,-74.050375,-74.051024,-74.051469,-74.052665,-74.054194,-74.055476,-74.056064,-74.057582,-74.062104,-74.069169, -74.074458, -74.077446,-74.078363,-74.078830,-74.079372,-74.081856,-74.085284, -74.089420));
+        
+       
+       
        String request= req.queryParams("number");
        String request1= req.queryParams("number1");
-       URL API = new URL("https://9i6rk1q1dl.execute-api.us-east-1.amazonaws.com/beta?IDBus="+request+"&Latitud="+4.755457+"&Longitud="+(-74.046128));
+       
+       for(int i=0;i<20;i++) {
+    	//   System.out.println(Longitud.get(i));
+       
+    		   
+    	   URL API = new URL("https://9i6rk1q1dl.execute-api.us-east-1.amazonaws.com/beta?IDBus="+request+"&Latitud="+Latitud.get(i)+"&Longitud="+Longitud.get(i));
 		
         URLConnection con = API.openConnection();
         
@@ -67,7 +84,9 @@ public class SparkWeb {
                         result=result+line;
 			System.out.println(line); 
 		}
-        
+		
+		Thread.sleep(8000);
+       } 
         
         String pageContent
                 = "<!DOCTYPE html>"
